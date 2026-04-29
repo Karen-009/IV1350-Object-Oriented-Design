4c43c21d3c2e0d0ce2badc5959a1f43d04a90d53
ckaur <ckaur@kth.se>
2026-04-29 23:49:28 +0200

    Some changes in View so that main does not have an error

--- src/se/kth/iv1350/repairelectricbike/view/View.java
+++ src/se/kth/iv1350/repairelectricbike/view/View.java
@@ -1,5 +1,29 @@
 package se.kth.iv1350.repairelectricbike.view;
 
-public class View {
+import se.kth.iv1350.repairelectricbike.controller.Controller;
 
+/**
+ * This program has no real user interface. Instead, this
+ * class simulates user interactions with hardcoded calls to all system operations.
+ */
+public class View {
+    private Controller contr;
+
+    /**
+     * Creates a new instance.
+     *
+     * @param contr The controller used for all operations.
+     */
+    public View(Controller contr) {
+        this.contr = contr;
+    }
+
+    /**
+     * Simulates a user interaction that generates calls to
+     * all system operations in the basic flow. All returned
+     * values are printed to System.out.
+     */
+    public void sampleExecution() {
+    
+    }
 }
