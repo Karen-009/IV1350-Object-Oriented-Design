0ac07678d74e77d0584e5091f190d5349068f140
ckaur <ckaur@kth.se>
2026-04-29 14:51:55 +0200

    added comments to Controller

--- src/se/kth/iv1350/repairelectricbike/controller/Controller.java
+++ src/se/kth/iv1350/repairelectricbike/controller/Controller.java
@@ -36,6 +36,14 @@ public class Controller {
         this.printer = printer;
     }
 
+    /**
+     * Creates a new repair order and stores it in the repair order registry.
+     * 
+     * @param problemDesc   Description of the customer's reported problem.
+     * @param customerPhone Phone number of the customer placing the repair order.
+     * @param bikeSerialNo  Bike serial number of the bike associated with the repair order.
+     * @return  A DTO containing information about the created repair order.
+     */
     public RepairOrderDTO createRepairOrder(String problemDesc, String customerPhone, Bike bikeSerialNo) {
         repairOrder = new RepairOrder(problemDesc, customerPhone, bikeSerialNo);
         RepairOrderDTO dto = repairOrder.getRepairOrderDTO();
@@ -43,24 +51,53 @@ public class Controller {
         return dto;
     }
 
+    /**
+     * Finds a customer by phone number
+     * 
+     * @param phoneNumber   The phone number used to identify the customer.
+     * @return  A DTO containing the customer information, or null if no customer is found.
+     */
     public CustomerDTO findCustomer(String phoneNumber) {
         return CustomerRegistry.findCustomer(phoneNumber);
     }
 
+    /**
+     * Finds a repair order connected to a specific customer phone number.    
+     * 
+     * @param phoneNumber   The phone number associated with the repair order
+     * @return  A DTO containing the matching repair order.
+     */
     public RepairOrderDTO findRepairOrder(String phoneNumber){
         return repairOrderRegistry.findRepairOrder(phoneNumber);
     }
 
+    /**
+     * Retrieves all repair orders.
+     * 
+     * @return  A list containing DTOs for all repair orders.
+     */
     public List<RepairOrderDTO> findAllRepairOrders() {
         return repairOrderRegistry.findAllRepairOrders();
     }
 
+    /**
+     * Adds a diagnostic result to a specific repair order.
+     * 
+     * @param repairOrderId The ID of the repair order to update.
+     * @param diagTaskResult    The diagnostic result to add.
+     */
     public void addDiagnosticResult(String repairOrderId, DiagnosticTaskDTO diagTaskResult) {
         repairOrder = repairOrderRegistry.findRepairOrderById(repairOrderId);
         repairOrder.addDiagnosticResult(diagTaskResult);
         repairOrderRegistry.updateRepairOrder(repairOrder.getRepairOrderDTO());
     }
 
+    /**
+     * Adds a repair task to a specific repair order.
+     * 
+     * @param repairOrderId The ID of the repair order to update.
+     * @param repairTask    The repair task to add.
+     */
     public void addRepairTask(String repairOrderId, RepairTaskDTO repairTask) {
         repairOrder.addRepairTask(repairTask);
         repairOrderRegistry.updateRepairOrder(repairOrder.getRepairOrderDTO());
